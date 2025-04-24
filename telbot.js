fc3e290c4b18efa3a2b1943f29ee0a185dba9037
mehsall1995 <mehsall1995@gmail.com>
2025-04-24 21:29:49 +0330

    modified:   telbot.js

--- telbot.js
+++ telbot.js
@@ -1,1 +1,11 @@
-console.log("telBot Executed")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Vercel!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
