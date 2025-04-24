dbd765f8d21478c0cef74f6106a1322f8b583f74
mehsall1995 <mehsall1995@gmail.com>
2025-04-24 21:41:33 +0330

    modified:   package-lock.json 	modified:   package.json

---
+++ api/sendTelegram.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const { message } = req.query;
+
+  if (!message) {
+    return res.status(400).send('Message parameter is required');
+  }
+
+  // Send a request to Telegram API
+  const url = `https://api.telegram.org/bot<7687887969:AAHrX0dIvnTZk_ZVh29xqbcGGsKSOcg77Wk>/sendMessage?chat_id=<shipanooly>&text=${encodeURIComponent(message)}`;
+
+  try {
+    await axios.get(url);
+    return res.status(200).send('Message sent');
+  } catch (error) {
+    return res.status(500).send('Failed to send message');
+  }
+};
