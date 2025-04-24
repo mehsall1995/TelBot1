2f9a6dacd14301aa1bde31352d5e2db95352798f
mehsall1995 <mehsall1995@gmail.com>
2025-04-24 21:48:30 +0330

    modified:   api/sendTelegram.js

--- api/sendTelegram.js
+++ api/sendTelegram.js
@@ -8,8 +8,7 @@ module.exports = async (req, res) => {
   }
 
   // Send a request to Telegram API
-  const url = `https://api.telegram.org/bot<7687887969:AAHrX0dIvnTZk_ZVh29xqbcGGsKSOcg77Wk>/sendMessage?chat_id=<@shipanooly>&text=${encodeURIComponent(message)}`;
-
+  const url = `https://api.telegram.org/bot7687887969:AAHrX0dIvnTZk_ZVh29xqbcGGsKSOcg77Wk/sendMessage?chat_id=@shipanooly&text=${encodeURIComponent(message)}`;
   try {
     await axios.get(url);
     return res.status(200).send('Message sent');
